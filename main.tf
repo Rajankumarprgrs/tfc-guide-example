a46f56572f101152bf13436638568aebbfbe63ee
Rajan Kumar <73093307+Rajankumarprgrs@users.noreply.github.com>
2020-11-15 22:24:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,23 @@
 # provider "random" {
 #   version = "2.2"
 # }
+
+
+# resource "random_pet" "table_name" {}
+
+# resource "aws_dynamodb_table" "tfc_example_table" {
+#   name = "${var.db_table_name}-${random_pet.table_name.id}"
+
+#   read_capacity  = var.db_read_capacity
+#   write_capacity = var.db_write_capacity
+#   hash_key       = "UUID"
+
+#   attribute {
+#     name = "UUID"
+#     type = "S"
+#   }
+# }
+
 terraform {
   required_providers {
     aws = {
@@ -18,22 +35,12 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  profile = "default"
+  region  = "us-east-1"
 }
 
-resource "random_pet" "table_name" {}
-
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
+resource "aws_instance" "example" {
+  #ami           = "ami-00ddb0e5626798373"
+  ami           = "ami-0885b1f6bd170450c"
+  instance_type = "t2.micro"
 }
-
-
